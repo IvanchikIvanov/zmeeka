6fc054f82b98cfb32cbdfccffae3cfda8e1d4a4d
MoonNodes_Inc <86493242+IvanchikIvanov@users.noreply.github.com>
2023-10-15 06:03:14 +0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-import * as PIXI from 'https://cdn.skypack.dev/pixi.js';
+import * as PIXI from 'pixi.js';
 
 const app = new PIXI.Application({ width: 800, height: 600, backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
